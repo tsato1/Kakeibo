1dc16e53880533759a2aadad6f45c50e020102fe
tsato1 <a8tb1117@gmail.com>
2018-03-03 17:35:53 -0800

    db migration code is added

--- app/src/main/java/com/kakeibo/DBAdapter.java
+++ app/src/main/java/com/kakeibo/DBAdapter.java
@@ -6,12 +6,14 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by T on 2015/09/16.
  */
 public class DBAdapter
 {
+    private static final String TAG = DBAdapter.class.getSimpleName();
     private static final String DATABASE_NAME = "kakeibo.db";
     private static final int DATABASE_VERSION = 1;
 
@@ -19,10 +21,13 @@ public class DBAdapter
     public static final String COL_ID = "_id";
     public static final String COL_AMOUNT = "amount";
     public static final String COL_CATEGORY = "category";
+    public static final String COL_CATEGORY_CODE = "category_code";
     public static final String COL_MEMO = "memo";
     public static final String COL_EVENT_D = "event_d";
     public static final String COL_EVENT_YM = "event_ym";
     public static final String COL_UPDATE_DATE = "update_date";
+
+    public static final String DATABASE_ALTER_STATEMENT_1 = "ALTER TABLE " + TABLE_ITEM + " ADD COLUMN " + COL_CATEGORY_CODE + " INTEGER DEFAULT 0;";
 
     protected final Context context;
     protected DatabaseHelper dbHelper;
@@ -35,8 +40,11 @@ public class DBAdapter
     }
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
+        private final Context _context;
+
         public DatabaseHelper (Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            this._context = context;
         }
 
         @Override
@@ -60,8 +68,37 @@ public class DBAdapter
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEM);
-            onCreate(db);
+            if (oldVersion < 2) {
+                //upgradeVersion2(db);
+            }
+
+            Log.d(TAG,"oldVersion="+oldVersion+" : newVersion="+newVersion);
+        }
+
+        private void upgradeVersion2(SQLiteDatabase db) {
+            db.execSQL(DATABASE_ALTER_STATEMENT_1);
+
+            Cursor c = db.query(DATABASE_NAME, new String[]{COL_CATEGORY, COL_CATEGORY_CODE} , null, null, null, null, null, null);
+            if (c.moveToFirst()) {
+                do {
+                    String catName = c.getString(c.getColumnIndex(DBAdapter.COL_CATEGORY));
+                    ContentValues values = new ContentValues();
+
+                    int colId = c.getInt(c.getColumnIndex(COL_ID));
+                    String[] defaultCategory = _context.getResources().getStringArray(R.array.defaultCategory);
+                    int catCode = 0;
+
+                    for (int i=0; i<defaultCategory.length; ++i) {
+                        if (catName.equals(defaultCategory[i])) {
+                            catCode = i;
+                        }
+                    }
+
+                    values.put(COL_CATEGORY_CODE, catCode);
+                    db.update(DATABASE_NAME, values, COL_ID+"=?", new String[] {String.valueOf(colId)});
+                } while (c.moveToNext());
+            }
+            c.close();
         }
     }
 
@@ -111,7 +148,29 @@ public class DBAdapter
         values.put(COL_EVENT_D, item.getEventD());
         values.put(COL_EVENT_YM, item.getEventYM());
         values.put(COL_UPDATE_DATE, item.getUpdateDate());
+        //values.put(COL_CATEGORY_CODE, item.getCategoryCode());
 
         db.insertOrThrow(TABLE_ITEM, null, values);
     }
 }
+
+//    SQLiteDatabase db = helper.getReadableDatabase();
+//
+//    String table = "table2";
+//    String[] columns = {"column1", "column3"};
+//    String selection = "column3 =?";
+//    String[] selectionArgs = {"apple"};
+//    String groupBy = null;
+//    String having = null;
+//    String orderBy = "column3 DESC";
+//    String limit = "10";
+//
+//    Cursor cursor = db.query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+//
+//    table: the name of the table you want to query
+//    columns: the column names that you want returned. Don't return data that you don't need.
+//    selection: the row data that you want returned from the columns (This is the WHERE clause.)
+//    selectionArgs: This is substituted for the ? in the selection String above.
+//    groupBy and having: This groups duplicate data in a column with data having certain conditions. Any unneeded parameters can be set to null.
+//    orderBy: sort the data
+//    limit: limit the number of results to return
