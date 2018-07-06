0599992fa9c8db0fb73e40890af38d92fa683c90
tsato1 <a8tb1117@gmail.com>
2018-07-06 01:05:45 -0700

    release api key moved out of the project

--- app/src/main/java/com/kakeibo/export/CreateFileInFolderActivity.java
+++ app/src/main/java/com/kakeibo/export/CreateFileInFolderActivity.java
@@ -1,5 +1,7 @@
 package com.kakeibo.export;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.google.android.gms.drive.DriveContents;
@@ -7,6 +9,8 @@ import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.MetadataChangeSet;
 
 import com.kakeibo.R;
+import com.kakeibo.Utilities;
+import com.kakeibo.settings.SettingsActivity;
 
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -17,6 +21,15 @@ import java.io.Writer;
  */
 public class CreateFileInFolderActivity extends BaseExportActivity {
     private static final String TAG = CreateFileInFolderActivity.class.getSimpleName();
+
+    private int mIntDateFormat;
+    private String mStrDateFormat;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadSharedPreference();
+    }
 
     @Override
     protected void onDriveClientReady() {
@@ -42,9 +55,11 @@ public class CreateFileInFolderActivity extends BaseExportActivity {
                         writer.write("Hello World!");
                     }
 
+                    String title = "Kakeibo_Export_" + Utilities.getTodaysDate(mStrDateFormat);
+
                     MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                            .setTitle("New file")
-                            .setMimeType("text/plain")
+                            .setTitle(title)
+                            .setMimeType("text/csv")
                             .setStarred(true)
                             .build();
 
@@ -57,5 +72,23 @@ public class CreateFileInFolderActivity extends BaseExportActivity {
                     Log.e(TAG, "Unable to create file", e);
                     showMessage(getString(R.string.file_create_error));
                 });
+    }
+
+    public void loadSharedPreference() {
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String f = pref.getString(SettingsActivity.PREF_KEY_DATE_FORMAT, Utilities.DATE_FORMAT_YMD);
+        mIntDateFormat = Integer.parseInt(f);
+
+        switch (mIntDateFormat) {
+            case 1: // MDY
+                mStrDateFormat = Utilities.getTodaysDate(Utilities.DATE_FORMAT_MDY);
+                break;
+            case 2: // DMY
+                mStrDateFormat = Utilities.getTodaysDate(Utilities.DATE_FORMAT_DMY);
+                break;
+            default:  // YMD
+                mStrDateFormat = Utilities.getTodaysDate(Utilities.DATE_FORMAT_YMD);
+        }
     }
 }
